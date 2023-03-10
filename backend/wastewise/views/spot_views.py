c76a4fc5d002c1d126c47a756a6db36be3694ff4
Hanna <hanna.kim.x@gmail.com>
2023-03-10 10:47:39 -0600

    add APIView from rest_framework

--- backend/wastewise/views/spot_views.py
+++ backend/wastewise/views/spot_views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from django.shortcuts import get_object_or_404
 from rest_framework import status, generics
+from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.exceptions import PermissionDenied
