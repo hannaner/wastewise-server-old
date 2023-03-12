29dda906d3fa0ce0951820cd97f1c341b3c3fada
Hanna <hanna.kim.x@gmail.com>
2023-03-12 18:10:58 -0500

    add item model

---
+++ backend/wastewise/models/item.py
@@ -0,0 +1,29 @@
+from django.db import models
+# Item model
+
+class Item(models.Model):
+    name = models.CharField(max_length=250)
+    quantity = models.CharField(max_length=100)
+    exp_date = models.DateField(auto_now=False)
+    spot_id = models.ForeignKey(
+        'Spot',
+        on_delete=models.CASCADE
+    )
+
+    created_at = models.DateTimeField(auto_add_now=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"""
+        Item: {self.name}
+        Quantity: {self.quantity}
+        Expiring on: {self.exp_date}
+        """
+    
+    def as_dict(self)
+        return {
+            'id': self.id,
+            'name': self.name,
+            'quantity': self.quantity,
+            'exp_date': self.exp_date,
+        }
