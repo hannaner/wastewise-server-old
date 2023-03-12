259cc366634876ccdfd646faf49efc7ca9be95a9
Hanna <hanna.kim.x@gmail.com>
2023-03-12 01:30:01 -0600

    add additional rest_framework settings for authentication

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -97,10 +97,12 @@ DATABASES = {
 # Rest frameworks for authentication
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.TokenAuthentication'
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated'
+        'rest_framework.permissions.IsAuthenticated',
     ]
 }
 
