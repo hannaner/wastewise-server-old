626de8039fe442785bf8d5031021f17fb77d7991
Hanna <hanna.kim.x@gmail.com>
2023-03-13 00:23:49 -0500

    fix spot model, redo migrations

--- backend/wastewise/models/spot.py
+++ backend/wastewise/models/spot.py
@@ -10,6 +10,9 @@ class Spot(models.Model):
         on_delete=models.CASCADE
     )
 
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
     def __str__(self):
         return f"Spot: {self.title} / Description: {self.description}"
     
