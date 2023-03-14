e4573c7bcfde86094efb64fa04f7dd8dd8d9ca5e
Hanna <hanna.kim.x@gmail.com>
2023-03-14 17:55:53 -0500

    add dependencies to deploy on render

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -14,6 +14,7 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
