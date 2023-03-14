85b0384b65a53d4026a990a43d6742fb8297832f
Hanna <hanna.kim.x@gmail.com>
2023-03-13 20:45:51 -0500

    remove BasicAuthentication and SessionAuthentication from REST_FRAMEWORK

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -97,8 +97,8 @@ DATABASES = {
 # Rest frameworks for authentication
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.BasicAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
+        # 'rest_framework.authentication.BasicAuthentication',
+        # 'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
